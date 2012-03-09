8bd1574f5b14499134dfd1d350709c96be58b8f1
Matt Wright <matt@localprojects.net>
2012-03-09 00:50:57 -0500

    Add unique indexes to user model

--- flask_security/datastore/mongoengine.py
+++ flask_security/datastore/mongoengine.py
@@ -9,13 +9,13 @@ class MongoEngineUserDatastore(UserDatastore):
         self.db = db
         
         class Role(db.Document, RoleMixin):
-            name = db.StringField(required=True, max_length=80)
+            name = db.StringField(required=True, unique=True, max_length=80)
             description = db.StringField(max_length=255)
               
         class User(db.Document, UserMixin):
-            username = db.StringField(max_length=255)
-            email = db.StringField(max_length=255)
-            password = db.StringField(max_length=120)
+            username = db.StringField(unique=True, max_length=255)
+            email = db.StringField(unique=True, max_length=255)
+            password = db.StringField(required=True, max_length=120)
             active = db.BooleanField(default=True)
             roles= db.ListField(db.ReferenceField(Role), default=[])
             created_at = db.DateTimeField()
