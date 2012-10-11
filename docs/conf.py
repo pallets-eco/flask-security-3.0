f96feaffa1e6356215bf1503cc560fee3d9af88c
Matt Wright <matt@nobien.net>
2012-10-11 17:34:32 -0400

    Bump version number to 1.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ copyright = u'2012, Matt Wright'
 # built documents.
 #
 # The short X.Y version.
-version = '1.3.0-dev'
+version = '1.5.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
