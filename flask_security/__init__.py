6f9869e9c207870cbd0327599b43cae56c517737
Matt Wright <matt@nobien.net>
2013-03-27 17:20:31 -0400

    import auth_required into top level package

--- flask_security/__init__.py
+++ flask_security/__init__.py
@@ -15,7 +15,7 @@ __version__ = '1.6.0'
 from .core import Security, RoleMixin, UserMixin, AnonymousUser, current_user
 from .datastore import SQLAlchemyUserDatastore, MongoEngineUserDatastore, PeeweeUserDatastore
 from .decorators import auth_token_required, http_auth_required, \
-     login_required, roles_accepted, roles_required
+     login_required, roles_accepted, roles_required, auth_required
 from .forms import ForgotPasswordForm, LoginForm, RegisterForm, \
      ResetPasswordForm, PasswordlessLoginForm, ConfirmRegisterForm
 from .signals import confirm_instructions_sent, password_reset, \
