c84c485493eefe686bd1826b2085f6d3bc9e2ffa
Matt Wright <matt@nobien.net>
2013-04-03 11:07:36 -0400

    Bump version number to 1.6.1

--- flask_security/__init__.py
+++ flask_security/__init__.py
@@ -10,7 +10,7 @@
     :license: MIT, see LICENSE for more details.
 """
 
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 
 from .core import Security, RoleMixin, UserMixin, AnonymousUser, current_user
 from .datastore import SQLAlchemyUserDatastore, MongoEngineUserDatastore, PeeweeUserDatastore
