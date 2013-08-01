f6b532103dd8df50cfe0c37a883b221e9036c5ba
Matt Wright <matt@nobien.net>
2013-08-01 16:27:32 -0400

    Bump version number to 1.6.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Security',
-    version='1.6.7',
+    version='1.6.8',
     url='https://github.com/mattupstate/flask-security',
     license='MIT',
     author='Matt Wright',
