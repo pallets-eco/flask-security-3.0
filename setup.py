910d80d5112766cf17b3bded895550906badf5f4
Bruno Rocha <rochacbruno@gmail.com>
2013-12-23 13:46:14 -0200

    Fix version to be able to merge

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Security',
-    version='1.6.10',
+    version='1.6.9',
     url='https://github.com/mattupstate/flask-security',
     license='MIT',
     author='Matt Wright',
