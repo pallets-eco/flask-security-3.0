d185407ac4caf5648ef4c12eab83fec81c307407
Matt Wright <matt@nobien.net>
2014-05-06 13:14:14 -0400

    Add mock X-Forwarded-For header in trackable tests

--- tests/test_trackable.py
+++ tests/test_trackable.py
@@ -17,12 +17,12 @@ def test_trackable_flag(app, client):
     e = 'matt@lp.com'
     authenticate(client, email=e)
     logout(client)
-    authenticate(client, email=e)
+    authenticate(client, email=e, headers={'X-Forwarded-For': '127.0.0.1'})
 
     with app.app_context():
         user = app.security.datastore.find_user(email=e)
         assert user.last_login_at is not None
         assert user.current_login_at is not None
         assert user.last_login_ip == 'untrackable'
-        assert user.current_login_ip == 'untrackable'
+        assert user.current_login_ip == '127.0.0.1'
         assert user.login_count == 2
