8b036f2a3e457b0edb97cded94950eefc1c1b967
Nick Greenfield <boyd.greenfield@gmail.com>
2014-09-26 11:08:58 -0700

    Prevent open redirects when a malformed URL is passed to ?next=
    Example: "/login?next=http:///google.com" (note 3rd slash)

--- flask_security/utils.py
+++ flask_security/utils.py
@@ -212,7 +212,7 @@ def validate_redirect_url(url):
         return False
     url_next = urlsplit(url)
     url_base = urlsplit(request.host_url)
-    if url_next.netloc and url_next.netloc != url_base.netloc:
+    if url_next.scheme and url_next.netloc != url_base.netloc:
         return False
     return True
 
