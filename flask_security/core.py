4d71dfeb61b8dd5370d9634177ceceae0c5ef86a
AlexV <asmodehn@gmail.com>
2015-06-18 10:50:44 +0900

    pep8 fixes

--- flask_security/core.py
+++ flask_security/core.py
@@ -187,14 +187,20 @@ _default_forms = {
 }
 
 
-## TMP : small fix to current_user to handle different flask-login's UserMixin version during transition
+# TMP : small fix to current_user to handle
+# different flask-login's UserMixin version during transition
 BaseUserMixin.is_authenticated_compat = property(
-    lambda self: self.is_authenticated() if callable(self.is_authenticated) else self.is_authenticated
+    lambda self:
+        self.is_authenticated() if callable(self.is_authenticated)
+        else self.is_authenticated
 )
 AnonymousUserMixin.is_authenticated_compat = property(
-    lambda self: self.is_authenticated() if callable(self.is_authenticated) else self.is_authenticated
+    lambda self:
+        self.is_authenticated() if callable(self.is_authenticated)
+        else self.is_authenticated
 )
 ####
+
 
 def _user_loader(user_id):
     return _security.datastore.find_user(id=user_id)
