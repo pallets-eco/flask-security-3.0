e67a8ea249f57835264118af87ad55312d39842f
Charlie Wolf <charlie@wolf.is>
2015-11-25 08:31:33 -0800

    remove trailing whitespace

--- flask_security/utils.py
+++ flask_security/utils.py
@@ -131,7 +131,7 @@ def verify_and_update_password(password, user):
     :param user: The user to verify against
     """
     if config_value('DISABLE_ENCRYPTION'):
-        return password == user.password        
+        return password == user.password
     if _pwd_context.identify(user.password) != 'plaintext':
         password = get_hmac(password)
     verified, new_password = _pwd_context.verify_and_update(password, user.password)
