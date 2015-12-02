8b1ab24341b0b4cd008ab19c76717fd8b482e81f
Matt Wright <matt@nobien.net>
2015-12-02 17:20:46 -0500

    Bump version number to 1.7.5

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class PyTest(TestCommand):
 
 setup(
     name='Flask-Security',
-    version='1.7.4',
+    version='1.7.5',
     url='https://github.com/mattupstate/flask-security',
     license='MIT',
     author='Matt Wright',
