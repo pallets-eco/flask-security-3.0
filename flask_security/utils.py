7c510afc1c850774e2b8c0132726286621a391fd
韦承飞 <yangjiaronga@163.com>
2017-03-03 09:51:46 +0800

    delete redirect url check

--- flask_security/utils.py
+++ flask_security/utils.py
@@ -220,10 +220,10 @@ def url_for_security(endpoint, **values):
 def validate_redirect_url(url):
     if url is None or url.strip() == '':
         return False
-    url_next = urlsplit(url)
-    url_base = urlsplit(request.host_url)
-    if (url_next.netloc or url_next.scheme) and url_next.netloc != url_base.netloc:
-        return False
+    # url_next = urlsplit(url)
+    # url_base = urlsplit(request.host_url)
+    # if (url_next.netloc or url_next.scheme) and url_next.netloc != url_base.netloc:
+    #     return False
     return True
 
 
