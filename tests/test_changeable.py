3dae7b705a44c0ccc89f252c4794f2a4a0a29ae6
Wells Lucas Santo <theonlydevking@gmail.com>
2017-05-29 05:16:30 -0400

    Do not use strip() to compare passwords in flask_security.forms.ChangePasswordForm (#642)
    * Do not use strip() to compare passwords.
      Currently in the password change form, strip() is used to
      compare old and new passwords. However, whitespace is an
      accepted part of a password. For example, '     a' and
      'a     ' are two different *valid* passwords. Thus, the
      strip() should not be done.
    
    * Add whitespace test case for password change.

--- tests/test_changeable.py
+++ tests/test_changeable.py
@@ -85,8 +85,17 @@ def test_recoverable_flag(app, client, get_message):
     assert len(outbox) == 1
     assert "Your password has been changed" in outbox[0].html
 
+    # Test leading & trailing whitespace not stripped
+    response = client.post('/change', data={
+        'password': 'newpassword',
+        'new_password': '      newpassword      ',
+        'new_password_confirm': '      newpassword      '
+    }, follow_redirects=True)
+    assert get_message('PASSWORD_CHANGE') in response.data
+
     # Test JSON
-    data = ('{"password": "newpassword", "new_password": "newpassword2", '
+    data = ('{"password": "      newpassword      ", '
+            '"new_password": "newpassword2", '
             '"new_password_confirm": "newpassword2"}')
     response = client.post(
         '/change',
