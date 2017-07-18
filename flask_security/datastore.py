fb73ac7d7681e23e5b7e47676ec9e9aa4a472016
Nuno Santos <nunofvsantos@gmail.com>
2017-07-18 08:51:25 +0200

    Make `roles` in user model query optimization "optional".

--- flask_security/datastore.py
+++ flask_security/datastore.py
@@ -234,18 +234,17 @@ class SQLAlchemyUserDatastore(SQLAlchemyDatastore, UserDatastore):
 
     def get_user(self, identifier):
         from sqlalchemy import func as alchemyFn
-        from sqlalchemy.orm import joinedload
+        user_model_query = self.user_model.query
+        if hasattr(self.user_model, 'roles'):
+            from sqlalchemy.orm import joinedload
+            user_model_query = user_model_query.options(joinedload('roles'))
+
         if self._is_numeric(identifier):
-            return self.user_model.query\
-                                  .options(joinedload('roles'))\
-                                  .get(identifier)
+            return user_model_query.get(identifier)
         for attr in get_identity_attributes():
             query = alchemyFn.lower(getattr(self.user_model, attr)) \
                 == alchemyFn.lower(identifier)
-            rv = self.user_model.query\
-                                .options(joinedload('roles'))\
-                                .filter(query)\
-                                .first()
+            rv = user_model_query.filter(query).first()
             if rv is not None:
                 return rv
 
@@ -257,11 +256,12 @@ class SQLAlchemyUserDatastore(SQLAlchemyDatastore, UserDatastore):
         return True
 
     def find_user(self, **kwargs):
-        from sqlalchemy.orm import joinedload
-        return self.user_model.query\
-                              .options(joinedload('roles'))\
-                              .filter_by(**kwargs)\
-                              .first()
+        query = self.user_model.query
+        if hasattr(self.user_model, 'roles'):
+            from sqlalchemy.orm import joinedload
+            query = query.options(joinedload('roles'))
+
+        return query.filter_by(**kwargs).first()
 
     def find_role(self, role):
         return self.role_model.query.filter_by(name=role).first()
