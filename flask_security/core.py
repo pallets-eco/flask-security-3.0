547ea3a057027b0bcec3688a3e3ca706bb487606
Morabaraba <morabaraba@openhazel.co.za>
2017-07-19 18:30:04 +0200

    fixed core.py:232:22: W291 trailing whitespace
    /home/travis/build/mattupstate/flask-security/flask_security/core.py:232:22: W291 trailing whitespace

--- flask_security/core.py
+++ flask_security/core.py
@@ -229,7 +229,7 @@ def _request_loader(request):
     if request.is_json:
         data = request.get_json(silent=True) or {}
         if isinstance(data, list):
-            data = {}        
+            data = {}
         token = data.get(args_key, token)
 
     try:
