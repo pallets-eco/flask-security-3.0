e1b0be1f04aed2876df32c69f7fd564a4ac7c601
Axel Haustant <noirbizarre@gmail.com>
2017-09-29 10:45:31 +0200

    Make SECURITY_PASSWORD_SINGLE_HASH a list of scheme ignoring double hash

--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -8,8 +8,9 @@
 
 from pytest import raises
 from utils import authenticate, init_app_with_options
+from passlib.hash import pbkdf2_sha256, django_pbkdf2_sha256, plaintext
 
-from flask_security.utils import encrypt_password, verify_password
+from flask_security.utils import encrypt_password, verify_password, get_hmac
 
 
 def test_verify_password_bcrypt_double_hash(app, sqlalchemy_datastore):
@@ -30,6 +31,37 @@ def test_verify_password_bcrypt_single_hash(app, sqlalchemy_datastore):
     })
     with app.app_context():
         assert verify_password('pass', encrypt_password('pass'))
+
+
+def test_verify_password_single_hash_list(app, sqlalchemy_datastore):
+    init_app_with_options(app, sqlalchemy_datastore, **{
+        'SECURITY_PASSWORD_HASH': 'bcrypt',
+        'SECURITY_PASSWORD_SALT': 'salty',
+        'SECURITY_PASSWORD_SINGLE_HASH': ['django_pbkdf2_sha256', 'plaintext'],
+        'SECURITY_PASSWORD_SCHEMES': [
+            'bcrypt', 'pbkdf2_sha256', 'django_pbkdf2_sha256', 'plaintext'
+        ]
+    })
+    with app.app_context():
+        # double hash
+        assert verify_password('pass', encrypt_password('pass'))
+        assert verify_password('pass', pbkdf2_sha256.hash(get_hmac('pass')))
+        # single hash
+        assert verify_password('pass', django_pbkdf2_sha256.hash('pass'))
+        assert verify_password('pass', plaintext.hash('pass'))
+
+
+def test_verify_password_backward_compatibility(app, sqlalchemy_datastore):
+    init_app_with_options(app, sqlalchemy_datastore, **{
+        'SECURITY_PASSWORD_HASH': 'bcrypt',
+        'SECURITY_PASSWORD_SINGLE_HASH': False,
+        'SECURITY_PASSWORD_SCHEMES': ['bcrypt', 'plaintext']
+    })
+    with app.app_context():
+        # double hash
+        assert verify_password('pass', encrypt_password('pass'))
+        # single hash
+        assert verify_password('pass', plaintext.hash('pass'))
 
 
 def test_verify_password_bcrypt_rounds_too_low(app, sqlalchemy_datastore):
@@ -58,13 +90,4 @@ def test_missing_hash_salt_option(app, sqlalchemy_datastore):
             'SECURITY_PASSWORD_HASH': 'bcrypt',
             'SECURITY_PASSWORD_SALT': None,
             'SECURITY_PASSWORD_SINGLE_HASH': False,
-        })
-
-
-def test_single_hash_should_have_no_salt(app, sqlalchemy_datastore):
-    with raises(RuntimeError):
-        init_app_with_options(app, sqlalchemy_datastore, **{
-            'SECURITY_PASSWORD_HASH': 'bcrypt',
-            'SECURITY_PASSWORD_SALT': 'salty',
-            'SECURITY_PASSWORD_SINGLE_HASH': True,
         })
